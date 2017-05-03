6df4df9a8dcdc6a7ff30cca1e3c628d7e9a57f4d
Hitesh C <jainhitesh9998@gmail.com>
2017-05-03 15:59:18 +0000

    algorithmic crush and input file

--- HackerRank/DataStructures/Arrays/AlgorithmicCrush_ineffective.cc
+++ HackerRank/DataStructures/Arrays/AlgorithmicCrush_ineffective.cc
@@ -3,18 +3,21 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <fstream>
 using namespace std;
 
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int n, m;
-    cin >>n>>m;
+		ifstream read("./input04.txt");
+    read >>n>>m;
     vector<long> arr(n, 0);
     
     for (int i = 0; i<m; i++){
         int a, b, v;
-        cin>>a>>b>>v;
+
+        read>>a>>b>>v;
         for (a; a<=b; a++){
             arr[a-1]+=v;
         }
