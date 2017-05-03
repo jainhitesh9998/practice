61937204ee38e0f05b80d3cd9c22f122a7968eaa
Hitesh C <jainhitesh9998@gmail.com>
2017-05-03 21:34:30 +0530

    Made changes in file

---
+++ HackerRank/DataStructures/Arrays/AlgorithmicCrush_ineffective.cc
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, m;
+    cin >>n>>m;
+    vector<long> arr(n, 0);
+    
+    for (int i = 0; i<m; i++){
+        int a, b, v;
+        cin>>a>>b>>v;
+        for (a; a<=b; a++){
+            arr[a-1]+=v;
+        }
+    }
+    cout<<*max_element(arr.begin(), arr.end());
+    return 0;
+    
+	
+}
